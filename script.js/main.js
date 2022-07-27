144d7f1fbac79f768e268400fded1439c02eea46
shaxzodkamolxojayev-098 <shaxzodkamolxojayev@gmail.com>
2022-07-27 16:50:46 +0500

    add code

---
+++ script.js/main.js
@@ -0,0 +1,10 @@
+var user = alert("error not 404!");
+var inrto = prompt("ismingiz", "shaxzod").trim;
+var inrto_1 = prompt("familiyangiz", "kamoxojayev") .trim;
+var inrto_2 = prompt("yoshingiz", "24") .trim;
+var inrto_3 = prompt("kasbingiz", "dasturchi") .trim;
+var inrto_4  = prompt("manzilingiz", "toshkent") .trim;
+var inrto_5 = confirm("rahmat!");
+
+console.log("user malumotlari: " + " " + inrto + " " + inrto_1 + " " + inrto_2 + " " + inrto_3 + " "
+ + inrto_4 + " " + inrto_5);
